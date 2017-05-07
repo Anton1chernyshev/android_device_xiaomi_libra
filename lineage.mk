02f36e5186e7f894d6610affb4753d5f3f73efc8
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-05-07 23:51:04 +0300

    libra: Add unofficial build ID

--- lineage.mk
+++ lineage.mk
@@ -26,3 +26,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="libra" \
     BUILD_FINGERPRINT="Xiaomi/libra/libra:7.0/NRD90M/V8.2.3.0.NXKCNEC:user/release-keys" \
     PRIVATE_BUILD_DESC="libra-user 7.0 NRD90M V8.2.3.0.NXKCNEC release-keys"
+
+# Unofficial build ID
+TARGET_UNOFFICIAL_BUILD_ID := HnT
