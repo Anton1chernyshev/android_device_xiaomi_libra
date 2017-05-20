267de82398e21c2ecce972ba6e7a947bda768e93
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-05-20 18:52:40 +0300

    Move some configs

--- product/misc.mk
+++ product/misc.mk
@@ -4,7 +4,9 @@ PRODUCT_PACKAGES += \
 
 # misc
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/misc,system/etc)
+    $(LOCAL_PATH)/configs/modem/Diag.cfg:system/etc/modem/Diag.cfg \
+    $(LOCAL_PATH)/configs/mcd_default.conf:system/etc/mcd_default.conf \
+    $(LOCAL_PATH)/configs/wifibt_diag.cfg:system/etc/wifibt_diag.cfg
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:system/etc/permissions/android.software.verified_boot.xml
