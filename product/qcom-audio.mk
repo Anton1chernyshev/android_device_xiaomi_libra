d63f70034d773ecff533035a098ca6a18d817bae
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-05-20 18:46:25 +0300

    Move audio configs to audio

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -9,6 +9,11 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/drc/drc_cfg_5.1.txt:system/etc/drc/drc_cfg_5.1.txt \
+    $(LOCAL_PATH)/audio/drc/drc_cfg_AZ.txt:system/etc/drc/drc_cfg_AZ.txt \
+    $(LOCAL_PATH)/audio/capability.xml:system/etc/capability.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+
+
