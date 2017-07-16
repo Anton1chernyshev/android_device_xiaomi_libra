e803c16d566af5ebcb4f9c075b4a146cb48cdfd0
Anton1chernyshev <anton1chernyshev@gmail.com>
2017-07-16 11:22:40 +0300

    cleanup

--- product/audio.mk
+++ product/audio.mk
@@ -33,5 +33,4 @@ PRODUCT_COPY_FILES += \
 
 # features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
